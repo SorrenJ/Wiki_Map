35d2c14eb7d978a270d5dd71714839a6958c70c3
SorrenJ <soalexjao@gmail.com>
2024-06-19 12:35:45 -0700

    added scehema tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,42 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS maps CASCADE;
+DROP TABLE IF EXISTS locations CASCADE;
+DROP TABLE IF EXISTS favorite_maps CASCADE;
+
 CREATE TABLE users (
+
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE maps (
+
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE locations (
+
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  image VARCHAR(255),
+  longitude DECIMAL,
+  latitude DECIMAL,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE favorite_maps (
+
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE
 );
