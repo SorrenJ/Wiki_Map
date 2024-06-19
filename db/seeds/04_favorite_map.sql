1fe85ae1ec69517203c0c5bfb7384f27ee7f46ac
SorrenJ <soalexjao@gmail.com>
2024-06-19 13:00:55 -0700

    add seed insertions: users, maps, locations, favorite_maps

---
+++ db/seeds/04_favorite_map.sql
@@ -0,0 +1,8 @@
+-- Favorite Map Insertions
+
+INSERT INTO favorite_maps (user_id, map_id) VALUES (1, 1);
+INSERT INTO favorite_maps (user_id, map_id) VALUES (1, 2);
+INSERT INTO favorite_maps (user_id, map_id) VALUES (2, 3);
+INSERT INTO favorite_maps (user_id, map_id) VALUES (2, 4);
+INSERT INTO favorite_maps (user_id, map_id) VALUES (3, 5);
+INSERT INTO favorite_maps (user_id, map_id) VALUES (3, 6);
