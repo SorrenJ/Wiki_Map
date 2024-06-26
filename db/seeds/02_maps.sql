aed2a3e699e1b022ea8e980e47f4c502cd0e52be
Bushra Patel <bpatel60@yahoo.com>
2024-06-25 20:39:29 -0400

    Add correct seeds to the maps db

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -8,31 +8,31 @@
 -- INSERT INTO maps (title, description, user_id) VALUES ('Crystal Caverns', 'A mesmerizing map of crystal caverns, filled with sparkling gems and hidden secrets.', 3);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Exploration of Ancient Ruins', 'Uncover the mysteries of a lost civilization.', 'https://example.com/ancient_ruins.jpg', 1);
+VALUES ('Restaurants', 'Enjoy delicious cuisine and a cozy atmosphere.', 'https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg', 1);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Ocean Adventure', 'Embark on a journey beneath the waves.', 'https://example.com/ocean_adventure.jpg', 2);
+VALUES ('Entertainment Venues', 'Experience thrilling performances and entertainment.', 'https://deadline.com/wp-content/uploads/2024/06/netflix-house-2.jpg?w=681&h=383&crop=1', 2);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Fantasy World Exploration', 'Discover mythical creatures and enchanted landscapes.', 'https://example.com/fantasy_world.jpg', 3);
+VALUES ('Resorts', 'Relax and unwind in luxury accommodation.', 'https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?cs=srgb&dl=pexels-thorsten-technoman-109353-338504.jpg&fm=jpg', 3);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Hidden Treasures', 'Seek out valuable artifacts in remote locations.', 'https://example.com/hidden_treasures.jpg', 1);
+VALUES ('Gas Stations', 'Find fuel and convenience on your journey.', 'https://headlight.news/wp-content/uploads/2024/03/Shell-Gas-Station.jpg', 1);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Space Exploration', 'Venture into the cosmos and explore distant planets.', 'https://example.com/space_exploration.jpg', 2);
+VALUES ('Cafes', 'Sip on coffee and enjoy a casual ambiance.', 'https://media.istockphoto.com/id/1400194993/photo/cappuccino-art.jpg?s=612x612&w=0&k=20&c=_nYOcyQ15cYEeUYgUzkC5qG946nkCwU06NiWKt1s8SE=', 2);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Medieval Kingdoms', 'Tour castles and villages of a bygone era.', 'https://example.com/medieval_kingdoms.jpg', 3);
+VALUES ('Beauty Salons', 'Indulge in beauty treatments and pampering.', 'https://thumbs.dreamstime.com/b/woman-facial-mask-beauty-salon-22945999.jpg', 3);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Island Paradise', 'Relax in scenic beaches and lush landscapes.', 'https://example.com/island_paradise.jpg', 1);
+VALUES ('Parks', 'Explore nature and enjoy outdoor activities.', 'https://images.squarespace-cdn.com/content/v1/57d6e71d8419c24ffcec7d6d/1566418149228-S5LV4AHKQPQ0ZSYKDV9L/IMG_3312+small.JPG?format=1500w', 1);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Jurassic Park', 'Encounter dinosaurs in their natural habitat.', 'https://example.com/jurassic_park.jpg', 2);
+VALUES ('Tourist Attractions', 'Visit iconic landmarks and cultural sites.', 'https://media.timeout.com/images/106038560/750/422/image.jpg', 2);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Cyberpunk Cityscape', 'Navigate neon-lit streets of a futuristic metropolis.', 'https://example.com/cyberpunk_cityscape.jpg', 3);
+VALUES ('Clothing Stores', 'Shop for trendy fashion and accessories.', 'https://media.istockphoto.com/id/901863672/photo/clothes-shop-interior.jpg?s=612x612&w=0&k=20&c=2Xv0bb-igj9sqzsZQgrnBAV3uxPTXmUpZ69MbiCYStY=', 3);
 
 INSERT INTO maps (title, description, thumbnail_photo_url, user_id)
-VALUES ('Underwater World', 'Explore coral reefs and marine life beneath the sea.', 'https://example.com/underwater_world.jpg', 1);
+VALUES ('Supermarkets', 'Stock up on groceries and household essentials.', 'https://cayugadisplays.com/img-cache/http/img/blog/9G4A1692.jpg/d93d811bbd8c23482c9fdbb067e3f883.jpg', 1);
